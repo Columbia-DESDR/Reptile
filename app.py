c7a8d7e3a666a1e50de8163719ee39e99aa57905
Zachary Huang <zhuang333@wisc.edu>
2020-03-30 11:35:20 -0400

    satellite data

--- app.py
+++ app.py
@@ -8,41 +8,46 @@ df = None
 app = flask.Flask(__name__,static_url_path='')
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+# # Create some test data for our catalog in the form of a list of dictionaries.
+# books = [
+#     {'id': 0,
+#      'title': 'A Fire Upon the Deep',
+#      'author': 'Vernor Vinge',
+#      'first_sentence': 'The coldsleep itself was dreamless.',
+#      'year_published': '1992'},
+#     {'id': 1,
+#      'title': 'The Ones Who Walk Away From Omelas',
+#      'author': 'Ursula K. Le Guin',
+#      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+#      'published': '1973'},
+#     {'id': 2,
+#      'title': 'Dhalgren',
+#      'author': 'Samuel R. Delany',
+#      'first_sentence': 'to wound the autumnal city.',
+#      'published': '1975'}
+# ]
 
 
 @app.route('/', methods=['GET'])
 def home():
     return  app.send_static_file('heatmap.html')
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    data = {
-        'hello'  : 'world',
-        'number' : 3
-    }
-    js = json.dumps(data)
-    resp = Response(js, status=200, mimetype='application/json')
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+
+@app.route('/satellite', methods=['GET'])
+def sate():
+    return  app.send_static_file('heatmap copy.html')
+
+# # A route to return all of the available entries in our catalog.
+# @app.route('/api/v1/resources/books/all', methods=['GET'])
+# def api_all():
+#     data = {
+#         'hello'  : 'world',
+#         'number' : 3
+#     }
+#     js = json.dumps(data)
+#     resp = Response(js, status=200, mimetype='application/json')
+#     resp.headers['Access-Control-Allow-Origin'] = '*'
+#     return resp
 
 @app.route('/api/db', methods=['POST'])
 def api_db():
