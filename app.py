a01e00c84fb2030cba8f3da89cb06e0560b4c076
abhyuday <ab5434@columbia.edu>
2023-08-09 13:37:58 -0400

    postgres sql changes

--- app.py
+++ app.py
@@ -487,4 +487,4 @@ def after_request(response):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
--- db.py
+++ db.py
@@ -1,64 +1,34 @@
-import duckdb
-import pandas as pd
-import sys
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import Config
+import pandas.io.sql as psql
 
 
+def kon_connection():
+    c = psycopg2.connect(user=Config.POSTGRES_CREDS["user"],
+                         password=Config.POSTGRES_CREDS["password"],
+                         host=Config.POSTGRES_CREDS["host"],
+                         port=Config.POSTGRES_CREDS["port"],
+                         database=Config.POSTGRES_CREDS["database"])
+    return c
 
-try:
-    # Initialize DuckDB
-    conn = duckdb.connect('./db/feedback.db')
-    # Create a table
-    conn.execute("""
-    CREATE TABLE feedback (
-        sid VARCHAR,
-        feedback_1990 VARCHAR,
-        feedback_1992 VARCHAR,
-        feedback_1993 VARCHAR,
-        feedback_1994 VARCHAR,
-        feedback_1995 VARCHAR,
-        feedback_1996 VARCHAR,
-        feedback_1997 VARCHAR,
-        feedback_1998 VARCHAR,
-        feedback_1999 VARCHAR,
-        feedback_2000 VARCHAR,
-        feedback_2001 VARCHAR,
-        feedback_2002 VARCHAR,
-        feedback_2003 VARCHAR,
-        feedback_2004 VARCHAR,
-        feedback_2005 VARCHAR,
-        feedback_2006 VARCHAR,
-        feedback_2007 VARCHAR,
-        feedback_2008 VARCHAR,
-        feedback_2009 VARCHAR,
-        feedback_2010 VARCHAR,
-        feedback_2011 VARCHAR,
-        feedback_2012 VARCHAR,
-        feedback_2013 VARCHAR,
-        feedback_2014 VARCHAR,
-        feedback_2015 VARCHAR,
-        feedback_2016 VARCHAR,
-        feedback_2017 VARCHAR,
-        feedback_2018 VARCHAR,
-        feedback_2019 VARCHAR,
-        feedback_2020 VARCHAR,
-        feedback_2021 VARCHAR,
-        feedback_2022 VARCHAR,
-        feedback_2023 VARCHAR,
-        q1 VARCHAR,
-        q2 VARCHAR,
-        q3 VARCHAR,
-        q4 VARCHAR,
-        name VARCHAR,
-        comment VARCHAR
-    )
-    """)
-    conn.close()
-except:
-    pass
+
+def close_connection(conn, cur):
+    if conn:
+        if cur:
+            cur.close()
+        conn.close()
+
+
+def connect():
+    conn = kon_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    print("Reconnected database!")
+    return conn, cur
+
 
 def insert_data(data):
-    
-    conn = duckdb.connect('./db/feedback.db')
+    conn, cur = connect()
     # Manually specify the order of columns in your database
     columns_order = ['sid'] + [f'{year}feedback' for year in range(1990, 2024) if year != 1991] + ['q1', 'q2', 'q3', 'q4', 'name', 'comment']
 
@@ -67,16 +37,17 @@ def insert_data(data):
 
     # Prepare SQL statement
     sql = f"""
-    INSERT INTO feedback VALUES ({values})
+    INSERT INTO reptile.feedback VALUES ({values})
     """
 
     # Execute the statement
-    conn.execute(sql)
+    cur.execute(sql)
     conn.commit()
-    conn.close()
+    close_connection(conn, cur)
+
 
 def read_data():
-    conn = duckdb.connect('./db/feedback.db')
-    df = conn.execute("SELECT * FROM feedback").fetch_df()
-    conn.close()
+    conn, cur = connect()
+    df = psql.read_sql("SELECT * FROM reptile.feedback", conn)
+    close_connection(conn, cur)
     return df
