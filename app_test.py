701d9cbf94f4e46c62539ca5ad6992349560d994
Dieter Joubert <dieterjoubert@gmail.com>
2024-10-17 20:56:06 -0400

    add testing (#38)

---
+++ app_test.py
@@ -0,0 +1,10 @@
+import pytest
+from app import app as flask_app
+
+@pytest.fixture
+def app():
+    return flask_app
+
+def test_get_templates(client):
+    response = client.get("/levelTemplate.html")
+    assert response.status_code == 200
