927aea5b6a7c3d04ca97eb347ac22f45ecb5bdf1
DieterJoubert <dieterjoubert@gmail.com>
2024-10-18 23:13:16 -0400

    hmm

--- validate_config_test.py
+++ validate_config_test.py
@@ -51,3 +51,4 @@ def test_validate_mock_config_with_no_password_throws_exception():
     with pytest.raises(jsonschema.ValidationError) as e:  
         validate_config(mock_config)
         print(e)
+        assert e == False
