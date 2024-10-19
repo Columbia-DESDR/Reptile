5faade238c0035d6beb85c1d42000e3a9dc7e48c
DieterJoubert <dieterjoubert@gmail.com>
2024-10-18 23:03:51 -0400

    update

--- validate_config.py
+++ validate_config.py
@@ -4,9 +4,36 @@ schema = {
     "type": "object",
     "properties": {
         "FEEDBACK_LEVEL": {"type": "string"},
-        "PASSWORD": {"type": "number"},
+        "PASSWORD": {"type": "string"},
+        "HIERARCHY": {
+            "type": "array",
+            "items": { "type": "string" },
+        },
+        "TIMESPAN": {
+            "type": "object",
+            "properties": {
+                "START": {"type": "number"},
+                "END": {"type": "number"},
+            }
+        },
+        "DISPLAY": {
+            "type": "object",
+            "properties": {
+                "INSTANCE_TITLE": {"type": "string"},
+                "TIME_NAME": {"type": "string"},
+                "NUMERICAL_NAME": {"type": "string"},
+                "COMMENT_NAME": {"type": "string"},
+            }
+        },
+        "COLORS": {
+            "type": "object",
+            "properties": {
+                "FARMERS": {"type": "string"},
+                "SATELLITE": {"type": "string"}
+            }
+        },
     },
-    "required": ["FEEDBACK_LEVEL", "PASSWORD"],
+    "required": ["FEEDBACK_LEVEL", "PASSWORD", "HIERARCHY", "TIMESPAN", "DISPLAY", "COLORS"],
 }
 
 def validate_config(config):
