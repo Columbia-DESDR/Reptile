00541f9fd8e08e226b7dd2f7ef6fe6a145863381
DieterJoubert <dieterjoubert@gmail.com>
2024-10-18 20:55:24 -0400

    mock config

--- validate_config_test.py
+++ validate_config_test.py
@@ -1,5 +1,42 @@
 from validate_config import validate_config
 
+mock_config = {
+    "DATA_SOURCES": {
+        "FILENAME": "./datasets/test/DRC_badyears_forzach.csv",
+        "SATELLITE_DATA": [
+            {
+                "NAME": "source_a",
+                "PATH": "./datasets/test/season_a.csv"
+            },
+            {
+                "NAME": "source_b",
+                "PATH": "./datasets/test/season_b.csv"
+            },
+            {
+                "NAME": "source_c",
+                "PATH": "./datasets/test/season_c.csv"
+            }
+        ]
+    },
+    "HIERARCHY": ["province", "sector", "village", "survey_id"],
+    "FEEDBACK_LEVEL": "sector",
+    "TIMESPAN": {
+        "START": 1990,
+        "END": 2023
+    },
+    "DISPLAY": {
+        "INSTANCE_TITLE": "Africa - Democratic Republic of the Congo",
+        "TIME_NAME": "year",
+        "NUMERICAL_NAME": "rank",
+        "COMMENT_NAME": "comments"
+    },
+    "COLORS": {
+        "FARMERS": "#9F2B68",
+        "SATELLITE": ""
+    },
+    "PASSWORD": "foobar"
+}
+
 def test_validate_config():
-    result = validate_config({'id': 1})
+    result = validate_config(mock_config)
     assert result == 'foo'
