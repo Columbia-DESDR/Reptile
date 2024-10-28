16a3fe2e2b97f9a21c1d88f470cce829f746d09e
DieterJoubert <dieterjoubert@gmail.com>
2024-10-28 17:28:49 -0400

    initial commit

--- app_test.py
+++ app_test.py
@@ -5,6 +5,14 @@ from app import app as flask_app
 def app():
     return flask_app
 
-def test_get_templates(client):
+def test_get_level_template(client):
     response = client.get("/levelTemplate.html")
+    assert response.status_code == 200
+
+def test_get_rec(client):
+    response = client.get("/api/getrec")
+    assert response.status_code == 200
+
+def test_get_rec_2(client):
+    response = client.get("/api/getrec2")
     assert response.status_code == 200
